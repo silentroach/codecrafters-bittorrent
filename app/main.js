aacaf4866e445f31a84376589ecd3d2a3ca1deae
Igor Kalashnikov <igor.kalashnikov@me.com>
2023-12-09 18:42:19 +0400

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -20,19 +20,15 @@ function decodeBencode(bencodedValue) {
 function main() {
   const command = process.argv[2];
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here!");
+  if (command === "decode") {
+    const bencodedValue = process.argv[3];
 
-  // Uncomment this block to pass the first stage
-  // if (command === "decode") {
-  //   const bencodedValue = process.argv[3];
-  //
-  //   // In JavaScript, there's no need to manually convert bytes to string for printing
-  //   // because JS doesn't distinguish between bytes and strings in the same way Python does.
-  //   console.log(JSON.stringify(decodeBencode(bencodedValue)));
-  // } else {
-  //   throw new Error(`Unknown command ${command}`);
-  // }
+    // In JavaScript, there's no need to manually convert bytes to string for printing
+    // because JS doesn't distinguish between bytes and strings in the same way Python does.
+    console.log(JSON.stringify(decodeBencode(bencodedValue)));
+  } else {
+    throw new Error(`Unknown command ${command}`);
+  }
 }
 
 main();
