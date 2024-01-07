0dd63edfa208c5d60ad182609e51f915e8eb0088
Igor Kalashnikov <igor.kalashnikov@me.com>
2024-01-07 21:13:16 +0400

    separate tracker class

--- app/main.ts
+++ app/main.ts
@@ -3,43 +3,7 @@ import { argv } from "node:process";
 import { decode } from "./bencoding";
 import { readTorrentFile } from "./torrent";
 import { stringifyBendecoded } from "./utils";
-
-const isUrlSafe = (char: string) => /[a-zA-Z0-9\-\._~]/.test(char);
-const urlencodeBuffer = (buf: Buffer): string => {
-  let encoded = "";
-  for (let i = 0; i < buf.length; i++) {
-    const charBuf = Buffer.from("00", "hex");
-    charBuf.writeUInt8(buf[i]);
-    const char = charBuf.toString();
-    // if the character is safe, then just print it, otherwise encode
-    if (isUrlSafe(char)) {
-      encoded += char;
-    } else {
-      encoded += `%${charBuf.toString("hex").toUpperCase()}`;
-    }
-  }
-  return encoded;
-};
-
-const parsePeers = (peers: Buffer): string[] => {
-  const result: string[] = [];
-
-  let idx = 0;
-  while (idx < peers.length) {
-    const peer = peers.subarray(idx, idx + 6);
-
-    const ip = Array.from({ length: 4 }, (_, idx) => peer.readUInt8(idx)).join(
-      "."
-    );
-
-    const port = peer.readUInt16BE(4);
-    result.push([ip, port].join(":"));
-
-    idx += 6;
-  }
-
-  return result;
-};
+import { Tracker } from "./tracker";
 
 const main = async () => {
   const command = argv[2];
@@ -68,30 +32,10 @@ ${torrent.pieceHashes.map((hash) => hash.toString("hex")).join("\n")}`);
 
       const torrent = await readTorrentFile(filename);
 
-      const url = new URL(torrent.announce);
-      url.searchParams.set("peer_id", "00112233445566778899");
-      url.searchParams.set("port", String(6881));
-      url.searchParams.set("uploaded", String(0));
-      url.searchParams.set("downloaded", String(0));
-      url.searchParams.set("left", String(torrent.length));
-      url.searchParams.set("compact", String(1));
+      const tracker = new Tracker(torrent.announce, "00112233445566778899");
+      const peers = await tracker.getPeers(torrent);
 
-      // safe bytes encoded, avoiding double encoding
-      url.search += `&info_hash=${urlencodeBuffer(torrent.infoHash)}`;
-
-      const response = await fetch(url);
-
-      const { "failure reason": error, ...other } = decode(
-        Buffer.from(await response.arrayBuffer())
-      ) as any;
-
-      if (error) {
-        console.error("Error:", error.toString());
-      } else if (other.peers) {
-        console.log(parsePeers(other.peers).join("\n"));
-      } else {
-        console.log({ ...other });
-      }
+      console.log(peers.join("\n"));
 
       break;
     }
--- app/tracker.ts
+++ app/tracker.ts
@@ -1,1 +1,82 @@
-export class Tracker {}
+import { decode } from "./bencoding";
+import { Torrent } from "./torrent";
+
+// @todo extract to separate file + write tests?
+const isUrlSafe = (char: string) => /[a-zA-Z0-9\-\._~]/.test(char);
+const urlencodeBuffer = (buf: Buffer): string => {
+  let encoded = "";
+  for (let i = 0; i < buf.length; i++) {
+    const charBuf = Buffer.from("00", "hex");
+    charBuf.writeUInt8(buf[i]);
+    const char = charBuf.toString();
+    // if the character is safe, then just print it, otherwise encode
+    if (isUrlSafe(char)) {
+      encoded += char;
+    } else {
+      encoded += `%${charBuf.toString("hex").toUpperCase()}`;
+    }
+  }
+  return encoded;
+};
+
+// @todo extract to separate file maybe
+const parsePeers = (peers: Buffer): string[] => {
+  const result: string[] = [];
+
+  let idx = 0;
+  while (idx < peers.length) {
+    const peer = peers.subarray(idx, idx + 6);
+
+    const ip = Array.from({ length: 4 }, (_, idx) => peer.readUInt8(idx)).join(
+      "."
+    );
+
+    const port = peer.readUInt16BE(4);
+    result.push([ip, port].join(":"));
+
+    idx += 6;
+  }
+
+  return result;
+};
+
+export class Tracker {
+  #announce: string;
+  #peerId: string;
+
+  constructor(announce: string, peerId: string) {
+    this.#announce = announce;
+    this.#peerId = peerId;
+  }
+
+  public async getPeers(torrent: Torrent): Promise<string[]> {
+    const url = new URL(this.#announce);
+    url.searchParams.set("peer_id", this.#peerId);
+    url.searchParams.set("port", String(6881));
+    url.searchParams.set("uploaded", String(0));
+    url.searchParams.set("downloaded", String(0));
+    url.searchParams.set("left", String(torrent.length));
+    url.searchParams.set("compact", "1");
+
+    // safe bytes encoded, avoiding double encoding
+    url.search += `&info_hash=${urlencodeBuffer(torrent.infoHash)}`;
+
+    const response = await fetch(url);
+
+    // @todo better code please
+
+    const { "failure reason": error, ...other } = decode(
+      Buffer.from(await response.arrayBuffer())
+    ) as Record<string, unknown>;
+
+    if (error) {
+      throw new Error(`Failed to get peers: ${error}`);
+    }
+
+    if (!Buffer.isBuffer(other.peers)) {
+      throw new Error(`Failed to decode peers`);
+    }
+
+    return parsePeers(other.peers);
+  }
+}
